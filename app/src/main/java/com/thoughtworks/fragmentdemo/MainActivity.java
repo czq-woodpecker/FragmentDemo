a5a5490b079b932cf2422311e52890ecfc9303aa
zeqiang.chen <1209120868@qq.com>
2020-08-07 18:23:55 +0800

    [zeqiang] feat: finish show AndroidFragment after click android button

--- app/src/main/java/com/thoughtworks/fragmentdemo/MainActivity.java
+++ app/src/main/java/com/thoughtworks/fragmentdemo/MainActivity.java
@@ -1,14 +1,74 @@
 package com.thoughtworks.fragmentdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button androidButton;
+    private Button javaButton;
+    private FragmentManager fragmentManager;
+    private Fragment androidFragment;
+    private boolean isAndroidButtonActivated;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        androidButton = findViewById(R.id.android_button);
+        javaButton = findViewById(R.id.java_button);
+
+        androidButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickAndroidButton();
+            }
+        });
+
+        javaButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickJavaButton();
+            }
+        });
+
+        androidFragment = new AndroidFragment();
+        fragmentManager = getSupportFragmentManager();
+
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.fragment_container, androidFragment);
+        fragmentTransaction.commit();
+        androidButton.setActivated(true);
+        this.isAndroidButtonActivated = true;
+    }
+
+    private void clickJavaButton() {
+        if(this.isAndroidButtonActivated) {
+            androidButton.setActivated(false);
+            this.isAndroidButtonActivated = false;
+            javaButton.setActivated(true);
+
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.remove(androidFragment);
+            fragmentTransaction.commit();
+        }
+    }
+
+    private void clickAndroidButton() {
+        if(!this.isAndroidButtonActivated) {
+            javaButton.setActivated(false);
+            androidButton.setActivated(true);
+            this.isAndroidButtonActivated = true;
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_container, androidFragment);
+            fragmentTransaction.commit();
+        }
     }
 }
